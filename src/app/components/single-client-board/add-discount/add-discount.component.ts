bab6216372bf7f80a592896a089b227388e1de7f
Amit Sahoo <kumaramit.sahoo@gmail.com>
2025-08-26 13:28:40 +0530

    add discount mobile view

--- src/app/components/single-client-board/add-discount/add-discount.component.ts
+++ src/app/components/single-client-board/add-discount/add-discount.component.ts
@@ -4,73 +4,102 @@ import { MatCheckboxChange } from '@angular/material/checkbox';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { ToastrService } from 'ngx-toastr';
 import { BillingService } from 'src/app/services/billing.service';
-
-
+import { BreakpointObserver } from '@angular/cdk/layout';
+import { StepperOrientation } from '@angular/material/stepper';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 @Component({
   selector: 'app-add-discount',
   templateUrl: './add-discount.component.html',
-  styleUrls: ['./add-discount.component.css']
+  styleUrls: ['./add-discount.component.css'],
 })
 export class AddDiscountComponent implements OnInit {
   @Output() billSettingFormData = new EventEmitter();
   billAmountDetails;
   billSettingForm = this.fb.group({
-    'isGstBill': [false, Validators.required],
-    'taxPercentage': [0, Validators.required],
-    'overallDiscountPercentage': [0, Validators.required]
+    isGstBill: [false, Validators.required],
+    taxPercentage: [0, Validators.required],
+    overallDiscountPercentage: [0, Validators.required],
   });
-  remarkFormControl = new FormControl(this.data.invoiceDetails.remarks, [Validators.required]);
+  remarkFormControl = new FormControl(this.data.invoiceDetails.remarks, [
+    Validators.required,
+  ]);
   billSettingFormValueChanged = false;
   tempBillSettingForm = this.billSettingForm.getRawValue();
 
-  constructor(private fb: FormBuilder, public dialogRef: MatDialogRef<AddDiscountComponent>,
-    @Inject(MAT_DIALOG_DATA) public data: any, private billingService: BillingService,
-    private toastrService: ToastrService) {
+  // 👇 responsive stepper orientation
+  stepperOrientation$: Observable<StepperOrientation>;
+
+  constructor(
+    private fb: FormBuilder,
+    public dialogRef: MatDialogRef<AddDiscountComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: any,
+    private billingService: BillingService,
+    private toastrService: ToastrService,
+    private breakpointObserver: BreakpointObserver
+  ) {
+    this.stepperOrientation$ = this.breakpointObserver
+      .observe('(max-width: 599px)')
+      .pipe(map((state) => (state.matches ? 'vertical' : 'horizontal')));
   }
 
   ngOnInit(): void {
-    if (this.data.invoiceDetails.taxPercentage && this.data.invoiceDetails.taxPercentage > 0) {
-      this.billSettingForm.get("taxPercentage").setValue(this.data.invoiceDetails.taxPercentage);
-      this.billSettingForm.get("taxPercentage").enable();
+    if (
+      this.data.invoiceDetails.taxPercentage &&
+      this.data.invoiceDetails.taxPercentage > 0
+    ) {
+      this.billSettingForm
+        .get('taxPercentage')!
+        .setValue(this.data.invoiceDetails.taxPercentage);
+      this.billSettingForm.get('taxPercentage')!.enable();
       this.tempBillSettingForm = this.billSettingForm.getRawValue();
     } else {
-      this.billSettingForm.get("taxPercentage").disable();
+      this.billSettingForm.get('taxPercentage')!.disable();
     }
-    if (this.data.invoiceDetails.discountPercentage && this.data.invoiceDetails.discountPercentage > 0) {
-      this.billSettingForm.get("overallDiscountPercentage").setValue(this.data.invoiceDetails.discountPercentage);
+
+    if (
+      this.data.invoiceDetails.discountPercentage &&
+      this.data.invoiceDetails.discountPercentage > 0
+    ) {
+      this.billSettingForm
+        .get('overallDiscountPercentage')!
+        .setValue(this.data.invoiceDetails.discountPercentage);
       this.tempBillSettingForm = this.billSettingForm.getRawValue();
     }
+
     this.billSettingFormData.emit(this.billSettingForm);
+
     this.billSettingForm.valueChanges.subscribe(() => {
-      if (this.tempBillSettingForm.taxPercentage !== this.billSettingForm.get("taxPercentage").value
-        || this.tempBillSettingForm.overallDiscountPercentage !== this.billSettingForm.get("overallDiscountPercentage").value)
-        this.billSettingFormValueChanged = true;
-      else
-        this.billSettingFormValueChanged = false;
+      this.billSettingFormValueChanged =
+        this.tempBillSettingForm.taxPercentage !==
+          this.billSettingForm.get('taxPercentage')!.value ||
+        this.tempBillSettingForm.overallDiscountPercentage !==
+          this.billSettingForm.get('overallDiscountPercentage')!.value;
+
       this.billSettingFormData.emit(this.billSettingForm);
     });
   }
 
   gstSelectedEvent(event: MatCheckboxChange) {
     if (!event.checked) {
-      this.billSettingForm.get("taxPercentage").disable();
-      this.billSettingForm.get("taxPercentage").setValue(0);
+      this.billSettingForm.get('taxPercentage')!.disable();
+      this.billSettingForm.get('taxPercentage')!.setValue(0);
     } else {
-      this.billSettingForm.get("taxPercentage").enable();
+      this.billSettingForm.get('taxPercentage')!.enable();
     }
   }
 
   onSave() {
-    this.billingService.addDiscountToBill(this.data.invoiceDetails.clientId, this.data.invoiceDetails.invoiceId, this.billingService.getBillAmountDetails(), this.remarkFormControl.value)
-      .subscribe(data => {
-        const result = {
-          "success": true,
-          "response": data
-        }
-        this.dialogRef.close(result);
-      }, error => {
-        this.toastrService.error(error.error.error);
+    this.billingService
+      .addDiscountToBill(
+        this.data.invoiceDetails.clientId,
+        this.data.invoiceDetails.invoiceId,
+        this.billingService.getBillAmountDetails(),
+        this.remarkFormControl.value
+      )
+      .subscribe({
+        next: (data) => this.dialogRef.close({ success: true, response: data }),
+        error: (err) => this.toastrService.error(err.error.error),
       });
   }
-
 }
